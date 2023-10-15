422f1b237bb5319b5f2915561984aa6fccc60d96
Gorgees Odisho <gorgees041@gmail.com>
2023-10-15 03:05:50 +0300

    PROPS ASSIGNMENT DONE

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,20 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  // let isBio;
+  // if (!props.bio || props.bio === undefined ) {
+  //   return null
+  // } else {
+  //   isBio = <p>{props.bio}</p>
+  // }
+  
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {(!props.bio || props.bio === undefined ) ? null : <p>{props.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
 }
